a47ff026487b6a841526d4780eaebd67aa3c7e40
Majcew <48514316+Majcew@users.noreply.github.com>
2020-01-29 13:36:33 +0100

    poprawka dla Cb

--- app/src/main/java/com/example/waluty/view/checkDays.kt
+++ app/src/main/java/com/example/waluty/view/checkDays.kt
@@ -1,17 +1,11 @@
 package com.example.waluty.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.waluty.R
 import com.example.waluty.model.DBHelper
 import kotlinx.android.synthetic.main.activity_check_days.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.check_layout.*
-import kotlinx.android.synthetic.main.check_layout.date_check
-import kotlinx.android.synthetic.main.check_layout.shortcut_check
-import kotlinx.android.synthetic.main.currency_layout.*
 
 class checkDays : AppCompatActivity() {
     private lateinit var adapter : NeededCurrencyAdapter
