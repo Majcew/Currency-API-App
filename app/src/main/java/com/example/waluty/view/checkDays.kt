946a10b936541dba44e6171f70294c67e12b9346
Majcew <48514316+Majcew@users.noreply.github.com>
2020-01-29 11:51:36 +0100

    2 aktywnosc z recyclerview zrobione, trzeba usuwać z bazy danych rekordy bo za dużo ich jest

--- app/src/main/java/com/example/waluty/view/checkDays.kt
+++ app/src/main/java/com/example/waluty/view/checkDays.kt
@@ -3,29 +3,31 @@ package com.example.waluty.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.waluty.R
 import com.example.waluty.model.DBHelper
+import kotlinx.android.synthetic.main.activity_check_days.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.check_layout.*
 import kotlinx.android.synthetic.main.check_layout.date_check
 import kotlinx.android.synthetic.main.check_layout.shortcut_check
 import kotlinx.android.synthetic.main.currency_layout.*
 
 class checkDays : AppCompatActivity() {
+    private lateinit var adapter : NeededCurrencyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.check_layout)
+        setContentView(R.layout.activity_check_days)
 
         val intent = intent
-        val mid = intent.getStringExtra("zl")
         val code = intent.getStringExtra("code")
-        val currency = intent.getStringExtra("currency")
-        val date = intent.getStringExtra("date")
         val db = DBHelper(this)
-        val kupa = db.neededCurrency(code)
-        currency_name_check.setText(kupa[0].name)
-        date_check.setText(kupa[0].date)
-        currency_kurs_check.setText(kupa[0].mid.toString())
-        shortcut_check.setText(kupa[0].code)
+        val needed = db.neededCurrency(code)
+
+        adapter = NeededCurrencyAdapter(needed) //jezeli jest nullem to random
+        recyclerViewCheck.adapter= adapter
+        recyclerViewCheck.layoutManager= LinearLayoutManager(this)
+
     }
 }
